68b420a26550ff6bff03ff31e27d0647427d178d
okadabasso <okadabasso@gmail.com>
2025-09-25 03:29:31 +0900

    Rename ConsoleApp1 to Experimental1 (move project files and update solution)

---
+++ Experimental1/Program.cs
@@ -0,0 +1,30 @@
+using Experimental1.Data;
+using Experimental1.Commands;
+using Experimental1.Filters;
+using ConsoleAppFramework;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using NLog;
+using NLog.Extensions.Logging;
+// create a new Console Application
+var app = ConsoleApp.Create()
+    .ConfigureDefaultConfiguration()
+    .ConfigureLogging((config, logging) =>
+    {
+        NLog.LogManager.Configuration = new NLogLoggingConfiguration(config.GetSection("nlog"));
+        // LogManager.Configuration.
+        logging.ClearProviders();
+        logging.AddNLog();
+    })
+    .ConfigureServices((services) =>
+    {
+        // add services
+    })
+
+;
+
+// コンソールアプリケーションのフィルターを登録
+app.UseFilter<LoggingFilter>();
+// コマンドライン引数を解析し、コンソールアプリケーションを実行
+app.Run(args);
